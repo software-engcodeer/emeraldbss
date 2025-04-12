3ae4acd539788b97806f7d61206d64114649f803
Jeck <jeck.labasan@gmail.com>
2025-04-12 23:43:38 +0800

    reinit project

--- .next/static/chunks/src_app_signup_page_tsx_0645e3f8._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "static/chunks/src_app_signup_page_tsx_0645e3f8._.js",
-    {},
-]);
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-  "path": "static/chunks/src_app_signup_page_tsx_0645e3f8._.js",
-  "chunks": [],
-  "source": "dynamic"
-});
